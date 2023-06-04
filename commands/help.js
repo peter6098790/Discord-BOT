592e73a4fac6a30fb6891a24065f8606cbbc2952
ChengEn <45934743+peter6098790@users.noreply.github.com>
2023-06-04 16:52:24 +0800

    更新 discord.js v14 語法

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,22 @@
-const Discord = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
-module.exports.run = async (bot, message, args) => {
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('指令列表'),
 
-    let helpEmbed = new Discord.RichEmbed()
-    .setDescription("指令列表")
-    .addField("機器人相關","!botinfo , !report(未開放)")
-    .addField("管理訊息","!cls")
-    .addField("個人訊息","!profile , !badge(未開放)")
-    .addField("貨幣經濟","!coins , !pay")
-    .addField("備註","!指令 help看更詳細用法")
-    .setColor("#8300ff");
+    async execute(interaction) {
+        let helpEmbed = new EmbedBuilder()
+        .setDescription("指令列表")
+        .addFields({ name: "機器人相關", value: "!botinfo , !report(未開放)"})
+        .addFields({ name: "管理訊息", value: "!cls"})
+        .addFields({ name: "個人訊息", value: "!profile , !badge(未開放)"})
+        .addFields({ name: "貨幣經濟", value: "!coins , !pay"})
+        .addFields({ name: "備註", value: "!指令 help看更詳細用法"})
+        .setColor("#8300ff");
 
-    message.channel.send(helpEmbed);
+        return interaction.reply({embeds: [helpEmbed]}); //'成功'
+    },
 }
 
 module.exports.help = {
--- commands/profile.js
+++ commands/profile.js
@@ -1,61 +1,56 @@
-const Discord = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const fs = require("fs");
 let contribution = require("../貢獻值.json");
 let badges = require("../badges.json");
 const range = ['菜鳥','偵查兵','步兵','士官','資深士官','一等士官','石衛士','血衛士','軍團士兵','百夫長','勇士','中將','將軍','督軍','高階督軍','大酋長'];
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('profile')
+        .setDescription('個人檔案'),
 
-module.exports.run = async (bot, message, args) => {
-    if(args[0]== "help"){
-        message.channel.send("!profile 顯示個人檔案,若無反應請先加入任一語音頻道初始化");
-        return;
-    }
-    let usercon = message.author.displayAvatarURL;
-    let uContribution = contribution[message.author.id].contribution;
-    let level = contribution[message.author.id].level;
-    
-    let needXp = (level+1)*100 - uContribution;
-    let init_rate = "□□□□□□□□□□";
-    let filled_rate = "■■■■■■■■■■";
-    let progress_rate = "";
-    //經驗等級>15時,稱號保持Lv15
-    if(level >= 15){
-        range[level] = range[15];
-    }
-    //升級所需經驗進度條
-    if(~~((needXp%100)/10)==0){
-        progress_rate = "[" + init_rate + "]";
-    }else{
-        progress_rate = "[" + filled_rate.substring(0,10-(~~((needXp%100)/10))) + init_rate.substring(0,~~((needXp%100)/10)) + "]"
-    }    
-    if(badges[message.author.id]){
-        let profileEmbed = new Discord.RichEmbed()
-        .setDescription("個人資料")
-        .setColor("#00FF00")
-        .setThumbnail(usercon)
-        .addField("玩家:",message.author.username)
-        .addField("貢獻值:",uContribution)
-        //.addField(`財富<:bal:570154707679445004>:`,coins[message.author.id].coins + `毛豆`)
-        .addField("榮譽軍階:" , range[level] + `(Lv.${level})` )
-        .addField("進度:", progress_rate)
-        .addField("徽章牆:" ,badges[message.author.id].badgeList.join(" "));
-        await message.channel.send(profileEmbed);
-    }else {
-        let profileEmbed = new Discord.RichEmbed()
-        .setDescription("個人資料")
-        .setColor("#00FF00")
-        .setThumbnail(usercon)
-        .addField("玩家:",message.author.username)
-        .addField("貢獻值:",uContribution)
-        //.addField(`財富<:bal:570154707679445004>:`,coins[message.author.id].coins + `毛豆`)
-        .addField("榮譽軍階:" , range[level] + `(Lv.${level})` )
-        .addField("進度:", progress_rate)
-        .addField("徽章牆:" ,"無");
-        await message.channel.send(profileEmbed);
-    }
-    
+    async execute(interaction) {
+        //console.log(interaction);
+        let usercon = interaction.user.avatar;
+        let userId = interaction.user.id;
+        let userName = interaction.user.username
+        let uContribution = contribution[userId].contribution;
+        let level = contribution[userId].level;
+
+        let needXp = (level+1)*100 - uContribution;
+        let init_rate = "□□□□□□□□□□";
+        let filled_rate = "■■■■■■■■■■";
+        let progress_rate = "";
+        //經驗等級>15時,稱號保持Lv15
+        if(level >= 15){
+            range[level] = range[15];
+        }
+
+        if(~~((needXp%100)/10)==0){
+            progress_rate = "[" + init_rate + "]";
+        }else{
+            progress_rate = "[" + filled_rate.substring(0,10-(~~((needXp%100)/10))) + init_rate.substring(0,~~((needXp%100)/10)) + "]"
+        }
+
+        let profileEmbed = new EmbedBuilder()
+            .setDescription("個人資料")
+            .setColor("#00FF00")
+            .setThumbnail(usercon)
+            .addFields({ name: "玩家:", value: userName})
+            .addFields({ name: "貢獻值:", value: uContribution.toString()})
+            .addFields({ name: "榮譽軍階:", value: range[level] + `(Lv.${level})`})
+            .addFields({ name: "升級進度:", value: progress_rate})
+            return interaction.reply({embeds: [profileEmbed]});
+        // 有無徽章
+        /* if(badges[interaction.user.id]){
+            profileEmbed.addFields({ name: "徽章牆:", value: badges[userId].badgeList.join(" ")});
+            return interaction.reply({embeds: [profileEmbed]});
+        }else {
+            profileEmbed.addFields({ name: "徽章牆:", value: "無"});
+            return interaction.reply({embeds: [profileEmbed]});
+        } */
+    },
 }
-
 module.exports.help = {
     name: "profile"
 }
